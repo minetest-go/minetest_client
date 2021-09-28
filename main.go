d741cbd4aa23e3d339f3bfa9ab484c39cf1473c1
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-09-28 11:32:28 +0200

    flags

--- main.go
+++ main.go
@@ -8,24 +8,28 @@ import (
 )
 
 func main() {
-	host := flag.String("host", "127.0.0.1", "The hostname")
-	port := flag.Int("port", 30000, "The portname")
-	help := flag.Bool("help", false, "Shows the help")
-	username := flag.String("username", "test", "The username")
-	password := flag.String("password", "enter", "The password")
+	var host, username, password string
+	var port int
+	var help bool
+
+	flag.StringVar(&host, "host", "127.0.0.1", "The hostname")
+	flag.IntVar(&port, "port", 30000, "The portname")
+	flag.BoolVar(&help, "help", false, "Shows the help")
+	flag.StringVar(&username, "username", "test", "The username")
+	flag.StringVar(&password, "password", "enter", "The password")
 	flag.Parse()
 
-	if *help {
+	if help {
 		flag.Usage()
 		return
 	}
 
-	client := NewClient(*host, *port)
+	client := NewClient(host, port)
 	client.AddListener(ClientAckHandler{})
 
 	ch := &ClientHandler{
-		Username: *username,
-		Password: *password,
+		Username: username,
+		Password: password,
 	}
 	client.AddCommandListener(ch)
 
